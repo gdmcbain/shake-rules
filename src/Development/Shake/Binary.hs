a2cffbf534606f62915b101708bbd697dd7272e7
G. D. McBain <gdmcbain@protonmail.com>
2018-06-26 11:35:40 +1000

    haddock

--- src/Development/Shake/Binary.hs
+++ src/Development/Shake/Binary.hs
@@ -1,11 +1,18 @@
+{-|
+  Files without extensions are tricky.  A common example is binaries in systems
+  derived from Unix.  Adding to the trickiness in this case is that the
+  extension varies between system.
+  
+  ‘Conditionally using the .exe extension with the Shake build system’
+
+  http://stackoverflow.com/questions/18712433/conditionally-using-the-exe-extension-with-the-shake-build-system
+-}
+
 module Development.Shake.Binary (namesBinary) where
 
 import Development.Shake
 import Development.Shake.FilePath
 
--- ‘Conditionally using the .exe extension with the Shake build system’
-
--- http://stackoverflow.com/questions/18712433/conditionally-using-the-exe-extension-with-the-shake-build-system
-
+-- | True as the extension matches that for a binary on the system.
 namesBinary :: FilePath -> Bool
 namesBinary = (== exe) . drop 1 . takeExtension
